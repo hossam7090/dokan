06ef5251d357bb7052f34897ac3b23b40ee905cd
abdo <sd>
2022-08-22 03:41:27 +0200

    full desgin

--- app/src/main/java/com/example/aldokan/adminpag.java
+++ app/src/main/java/com/example/aldokan/adminpag.java
@@ -1,17 +1,81 @@
 package com.example.aldokan;
 
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.AutoCompleteTextView;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
 
 public class adminpag extends AppCompatActivity {
-     AutoCompleteTextView autoCompleteTextView;
+    ImageView imageView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_adminpag);
 
+
+        Button save = findViewById(R.id.button8);
+        Button gallery = findViewById(R.id.gallery);
+        imageView = findViewById(R.id.imageView2);
+        EditText editText1 = findViewById(R.id.editTextTextPersonId);
+        EditText editText2 = findViewById(R.id.editTextTextPersoncatagory);
+        EditText editText3 = findViewById(R.id.editTextTextPersonPrice);
+        EditText editText4 = findViewById(R.id.editTextTextPersonName);
+        gallery.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                startActivityForResult(intent , 3);
+            }
+        });
+
+        save.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                editText1.setText("");
+                editText2.setText("");
+                editText3.setText("");
+                editText4.setText("");
+                imageView.setImageURI(null);
+            }
+        });
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode == RESULT_OK && data != null) {
+            Uri selectedImage = data.getData();
+
+            imageView.setImageURI(selectedImage);
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(@NonNull Menu menu) {
+        getMenuInflater().inflate(R.menu.message_menu,menu);
+        return true;
+
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        Intent M=new Intent(getApplicationContext(),message.class);
+        startActivity(M);
+        return true;
     }
 }
